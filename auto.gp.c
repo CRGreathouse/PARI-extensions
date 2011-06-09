779dda2076c39c68e7fdf6480d112e78010414f7
Charles Greathouse <crgreathouse@gmail.com>
2011-06-09 00:11:05 -0400

    Un-gp2c graeffe.

--- auto.gp.c
+++ auto.gp.c
@@ -5154,37 +5154,39 @@ DickmanRho(GEN x, long prec)
 }
 
 
+// Assumes that f is a t_POL with t_INT coefficients.  Returns a polynomial
+// with roots that are precisely the squares of the roots of f.
 GEN
 graeffe(GEN f)
 {
-  pari_sp ltop = avma;
-  GEN d = gen_0, g = gen_0, h = gen_0, p1 = gen_0;
-  GEN p2 = gen_0;	  /* vec */
-  GEN p3 = gen_0;
-  GEN p4 = gen_0;	  /* vec */
-  GEN x = pol_x(fetch_user_var("x")), p5 = gen_0;
-  if (typ(f) != t_POL)
-    pari_err(typeer, "graeffe");
-  d = stoi(degpol(f));
-  p1 = gaddgs(gdiventgs(d, 2), 1);
-  {
-    long i;
-    p2 = cgetg(gtos(p1)+1, t_VEC);
-    for (i = 1; gcmpsg(i, p1) <= 0; ++i)
-      gel(p2, i) = polcoeff0(f, (2*i) - 2, -1);
-  }
-  g = p2;
-  p3 = gdiventgs(gaddgs(d, 1), 2);
-  {
-    long i;
-    p4 = cgetg(gtos(p3)+1, t_VEC);
-    for (i = 1; gcmpsg(i, p3) <= 0; ++i)
-      gel(p4, i) = polcoeff0(f, (2*i) - 1, -1);
-  }
-  h = p4;
-  p5 = gsub(gsqr(gtopolyrev(g, -1)), gmul(x, gsqr(gtopolyrev(h, -1))));
-  p5 = gerepileupto(ltop, p5);
-  return p5;
+	pari_sp ltop = avma;
+	GEN d = gen_0, g = gen_0, h = gen_0, p1 = gen_0;
+	GEN p2 = gen_0;	  /* vec */
+	GEN p3 = gen_0;
+	GEN p4 = gen_0;	  /* vec */
+	GEN x = pol_x(fetch_user_var("x")), p5 = gen_0;
+	if (typ(f) != t_POL)
+		pari_err(typeer, "graeffe");
+	d = stoi(degpol(f));
+	p1 = gaddgs(gdiventgs(d, 2), 1);
+{
+	long i;
+	p2 = cgetg(gtos(p1)+1, t_VEC);
+	for (i = 1; gcmpsg(i, p1) <= 0; ++i)
+		gel(p2, i) = polcoeff0(f, (2*i) - 2, -1);
+}
+	g = p2;
+	p3 = gdiventgs(gaddgs(d, 1), 2);
+{
+	long i;
+	p4 = cgetg(gtos(p3)+1, t_VEC);
+	for (i = 1; gcmpsg(i, p3) <= 0; ++i)
+		gel(p4, i) = polcoeff0(f, (2*i) - 1, -1);
+}
+	h = p4;
+	p5 = gsub(gsqr(gtopolyrev(g, -1)), gmul(x, gsqr(gtopolyrev(h, -1))));
+	p5 = gerepileupto(ltop, p5);
+	return p5;
 }
 
 
