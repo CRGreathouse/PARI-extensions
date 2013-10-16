3245d440ed7cb3397cc40083622410c63fd803d9
Charles Greathouse <crgreathouse@gmail.com>
2013-10-16 00:51:27 -0400

    Add private function workaround.

--- othergpincludes.h
+++ othergpincludes.h
@@ -1,6 +1,30 @@
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef S_SPLINT_S
 
+static void
+ensure_nb(GEN L, long l)
+{
+  long nmax = list_nmax(L);
+  GEN v;
+  if (l <= nmax) return;
+  if (nmax)
+  {
+    nmax <<= 1;
+    if (l > nmax) nmax = l;
+    v = (GEN)pari_realloc(list_data(L), (nmax+1) * sizeof(long));
+  }
+  else /* unallocated */
+  {
+    nmax = 32;
+    if (list_data(L))
+      pari_err(e_MISC, "store list in variable before appending elements");
+    v = (GEN)pari_malloc((nmax+1) * sizeof(long));
+    v[0] = evaltyp(t_VEC) | _evallg(1);
+  }
+  list_data(L) = v;
+  list_nmax(L) = nmax;
+}
+
 /* return a value <= (48 << 10) = 49152 < primelinit */
 static ulong
 utridiv_bound(ulong n)
