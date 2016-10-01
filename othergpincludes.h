0364d1c0da00dec4cd7c600930635515407c790d
Charles R Greathouse IV <crgreathouse@gmail.com>
2016-10-01 18:11:21 -0400

    Use __builtin_popcountl.

--- othergpincludes.h
+++ othergpincludes.h
@@ -3,9 +3,9 @@ INLINE long hamming_word(ulong w) __attribute__ ((pure));
 INLINE long
 hamming_word(ulong w)
 {
-#if 0
+#if 1
   return __builtin_popcountl(w);
-#endif
+#else
   static long byte_weight[] = {
     0,1,1,2,1,2,2,3,1,2,2,3,2,3,3,4,1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,
     1,2,2,3,2,3,3,4,2,3,3,4,3,4,4,5,2,3,3,4,3,4,4,5,3,4,4,5,4,5,5,6,
@@ -19,6 +19,7 @@ hamming_word(ulong w)
   long sum = 0;
   while (w) { sum += byte_weight[w & 255]; w >>= 8; }
   return sum;
+#endif
 }
 
 #ifndef S_SPLINT_S
