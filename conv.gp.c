3b0cdbdb64cac5549baf11a55dcfcacd51a49ae8
CRGreathouse <crgreathouse@gmail.com>
2017-01-30 15:23:45 -0500

    fix spacing

--- conv.gp.c
+++ conv.gp.c
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/**														 Convenience																	**/
+/**                             Convenience                                  **/
 /******************************************************************************/
 
 void
@@ -20,7 +20,7 @@ gtor(GEN x, const char* funcName, long prec)
 {
     switch (typ(x)) {
         case t_REAL:
-            return x;	// x, not a copy of x
+            return x;  // x, not a copy of x
         case t_INT:
         case t_FRAC:
             return cxcompotor(x, prec);
@@ -51,86 +51,82 @@ gToC(GEN n)
 const char*
 toC(GEN n)
 {
-    long t = typ(n);
+  long t = typ(n);
 
-	/* Handle types other than t_INT */
-    if (t == t_FRAC)
-    {
-        GEN num = gel(n, 1), den = gel(n, 2);
-        if (equali1(num)) {
-            if (cmpis(den, 2) == 0) return "ghalf";
-            return pari_sprintf("ginv(%s)", toC(den));
-        }
-        return pari_sprintf("Qdivii(%s, %s)", toC(num), toC(den));
+  /* Handle types other than t_INT */
+  if (t == t_FRAC) {
+    GEN num = gel(n, 1), den = gel(n, 2);
+    if (equali1(num)) {
+       if (cmpis(den, 2) == 0) return "ghalf";
+       return pari_sprintf("ginv(%s)", toC(den));
     }
-    if (t != t_INT)
-    {
-        pari_err_TYPE("toC", n);
-        __builtin_unreachable();
-    }
+    return pari_sprintf("Qdivii(%s, %s)", toC(num), toC(den));
+  }
+  if (t != t_INT) {
+    pari_err_TYPE("toC", n);
+    __builtin_unreachable();
+  }
 
-    if (abscmpiu(n, 3) < 0)
-    {
-        if (cmpis(n, 2) == 0)
-            return "gen_2";
-        else if (equali1(n))
-            return "gen_1";
-        else if (signe(n) == 0)
-            return "gen_0";
-        else if (equalim1(n))
-            return "gen_m1";
-        else if (equalis(n, -2))
-            return "gen_m2";
-        __builtin_unreachable();
-    }
-    if (ispow2(n)) return pari_sprintf("int2u(%ld)", expi(n));
+  /* Handle special values */
+  if (abscmpiu(n, 3) < 0) {
+    if (cmpis(n, 2) == 0)
+      return "gen_2";
+    else if (equali1(n))
+      return "gen_1";
+    else if (signe(n) == 0)
+      return "gen_0";
+    else if (equalim1(n))
+      return "gen_m1";
+    else if (equalis(n, -2))
+      return "gen_m2";
+    __builtin_unreachable();
+  }
+  
+  if (ispow2(n)) return pari_sprintf("int2u(%ld)", expi(n));
     
-    // words: number of 32-bit words in n.
+  // words: number of 32-bit words in n.
 #ifdef LONG_IS_64BIT
-    long words = (lgefint(n) - 2) << 1;
-    if ((ulong)*int_MSW(n) <= 0xFFFFFFFF)
-        words--;
+  long words = (lgefint(n) - 2) << 1;
+  if ((ulong)*int_MSW(n) <= 0xFFFFFFFF)
+    words--;
 #else
-    long words = lgefint(n) - 2;
+  long words = lgefint(n) - 2;
 #endif
 
-	/* 32 bits */
-	if (words == 1)
-    {
-        if (signe(n) > 0)
-            return pari_sprintf("utoipos(%Ps)", n);
-        else
-            return pari_sprintf("utoineg(%Ps)", absi(n));
-    }
+  /* 32 bits */
+  if (words == 1) {
+    if (signe(n) > 0)
+      return pari_sprintf("utoipos(%Ps)", n);
+    else
+      return pari_sprintf("utoineg(%Ps)", absi(n));
+  }
 
-	/* 64 bits */
-	if (words == 2 && signe(n) > 0) return pari_sprintf("uu32toi(%Ps, %Ps)", shifti(n, -32), remi2n(n, 32));
+  /* 64 bits */
+  if (words == 2 && signe(n) > 0) return pari_sprintf("uu32toi(%Ps, %Ps)", shifti(n, -32), remi2n(n, 32));
 
-	/* Handle negatives */
-	if (signe(n) < 0) return pari_sprintf("negi(%s)", toC(absi(n)));
+  /* Handle negatives */
+  if (signe(n) < 0) return pari_sprintf("negi(%s)", toC(absi(n)));
     
-    // Large numbers
-    // N.B., this requires sprintf rather than pari_sprintf.
-    size_t maxSize = 9 + countdigits(stoi(words)) + 12*words;
-    char* buffer = (char*)pari_malloc(maxSize*sizeof(char));
-    int index = sprintf(buffer, "mkintn(%ld", words);	// 7+len(words) characters
-    long i = words - 1;
-    pari_sp btop = avma, st_lim = stack_lim(btop, 1);
-    for (; i >= 0; i--)
-    {
+  // Large numbers
+  // N.B., this requires sprintf rather than pari_sprintf.
+  size_t maxSize = 9 + countdigits(stoi(words)) + 12*words;
+  char* buffer = (char*)pari_malloc(maxSize*sizeof(char));
+  int index = sprintf(buffer, "mkintn(%ld", words);  // 7+len(words) characters
+  long i = words - 1;
+  pari_sp btop = avma, st_lim = stack_lim(btop, 1);
+  for (; i >= 0; i--) {
 #ifdef LONG_IS_64BIT
-        ulong chunk = mod2BIL(n) & 0xffffffff;
+    ulong chunk = mod2BIL(n) & 0xffffffff;
 #else
-        ulong chunk = mod2BIL(n);
+    ulong chunk = mod2BIL(n);
 #endif
-        index += sprintf(buffer+index, ", %lu", chunk);	// 12 characters max
-        n = shifti(n, -32); // a faster approach would use int_nextW
-        if (low_stack(st_lim, stack_lim(btop, 1)))
-            gerepileall(btop, 1, &n);
-
-    }
-    sprintf(buffer+index, ")");	// 2 characters with \0
-    return buffer;
+    index += sprintf(buffer+index, ", %lu", chunk);  // 12 characters max
+    n = shifti(n, -32); // a faster approach would use int_nextW
+    if (low_stack(st_lim, stack_lim(btop, 1)))
+      gerepileall(btop, 1, &n);
+  }
+  sprintf(buffer+index, ")");  // 2 characters with \0
+  return buffer;
 }
 
 
