b87932205c092c545afd601f4ae5e2f54dafe9d1
Charles R Greathouse IV <crgreathouse@gmail.com>
2023-01-26 14:10:01 -0500

    Improved updating script

--- update.sh
+++ update.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/sh
 
+showChanges() {
+	if [ "$same" -eq 1 ]; then
+		# There are no changes in the CHANGES file
+		echo "Nothing documented in CHANGES"
+	else
+		# There are changes in the CHANGES file
+		echo "Documented changes:"
+		git diff $oldHEAD $newHEAD -- CHANGES
+	fi
+	echo "Number of commits: $commits"
+}
+
+# Check tune.h
+if [ -f Olinux-x86_64/tune.h ]; then
+	lastTuned=$(date -r Olinux-x86_64/tune.h +"%b %d %H:%M")
+	echo "Last tuned $lastTuned"
+else
+	echo "*** Warning: No tuning file detected; consider running"
+	echo "./Configure --tune"
+fi
+
 # Pull the latest source from git, recording the respective HEADs.
 oldHEAD=$(git rev-parse HEAD)
 git pull
-if [ $? -ne 0 ]; then
-	echo "Error $? with git pull, exiting."
-	exit 1
+err=$?
+if [ "$err" -ne 0 ]; then
+	echo "Error $err with git pull, exiting."
+	exit $err
 fi
 newHEAD=$(git rev-parse HEAD)
 
@@ -17,21 +39,42 @@ if [ "$newHEAD" = "$oldHEAD" ]; then
 fi
 
 # Show changes and number of commits
-echo "Documented changes:"
-git diff $oldHEAD $newHEAD -- CHANGES
+same=$(git rev-parse $oldHEAD:CHANGES $newHEAD:CHANGES | uniq | wc -l)
 commits=$(git rev-list $newHEAD ^$oldHEAD --pretty=oneline --count)
-echo "Number of commits: $commits"
+showChanges
 
 # Try to make, catching errors
 make all
-if [ $? -ne 0 ]; then
-	echo "Error $? with make all, exiting."
+err=$?
+if [ "$err" -ne 0 ]; then
+	echo "Error $err with make all, exiting."
 	echo "Possible fix: ./Configure --tune"
-	exit 1
+	exit $err
 fi
 
 # Check that the build was successful and, if so, install.
-make dobench && sudo make install
+make dobench
+err=$?
+if [ "$err" -ne 0 ]; then
+	echo "Error $err when running bench, build was unsuccessful!"
+	exit $err
+fi
+
+sudo true
+err=$?
+if [ "$err" -ne 0 ]; then
+	echo "Error $err with sudo; not able to install. Local build seems ok."
+	echo "Check the error and consider retrying sudo make install."
+	exit $err
+fi
+
+sudo make install
+err=$?
+if [ "$err" -ne 0 ]; then
+	echo "Error $err when installing, but local build seems ok."
+	echo "Check the error and consider retrying sudo make install."
+	exit $err
+fi
 
 # Show the user the changes and number of commits again.
 echo "Updated successfully. Documented changes:"
